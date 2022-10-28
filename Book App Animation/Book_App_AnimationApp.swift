057cd5935c181a6092e3ed692b5802b057d86e01
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2022-10-28 13:57:36 +0700

    Initial Commit

---
+++ Book App Animation/Book_App_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_App_AnimationApp.swift
+//  Book App Animation
+//
+//  Created by Adit on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Book_App_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book App Animation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Book App Animation
+//
+//  Created by Adit on 28/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
